2b5aa12f0a7f037383f832655b30305a28b72824
Treker Chen <treker.chen@gmail.com>
2025-01-15 21:34:21 +0800

    Revert "Update sensor.py"
    This reverts commit 4c8270f6c18fa76ad2542b8a7fb9e61172be40c8.

--- custom_components/jcihitachi_tw/sensor.py
+++ custom_components/jcihitachi_tw/sensor.py
@@ -74,9 +74,7 @@ class JciHitachiIndoorHumiditySensorEntity(JciHitachiEntity, SensorEntity):
         """Return the indoor humidity."""
         status = self.hass.data[DOMAIN][UPDATED_DATA].get(self._thing.name, None)
         if status:
-            if status.power == "off":
-                return HVACMode.OFF
-            return status.mode if hasattr(status, "mode") else None
+            return None if status.indoor_humidity == "unsupported" else status.indoor_humidity
         return None
 
     @property
